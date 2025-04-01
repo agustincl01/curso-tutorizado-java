13f137dd0080389561edf4dc3d13ef2c9302c25d
Agustin <agustin_cuelloluna01@hotmail.com>
2025-03-31 23:30:07 -0300

    primer package creado

--- src/Stringclass/Nombre.java
+++ src/Stringclass/Nombre.java
@@ -1,12 +1,7 @@
 public class Nombre {
     public static void main(String[] args) {
         String nombre1 = "Juan";
-        String nombre2 = "Juan";
-        String nombre3 = "Pedro";
         String nombre4 = "Marcos";
-        int tamaño1 = nombre1.length();
-        int tamaño2 = nombre2.length();
-        int tamaño3 = nombre3.length();
         int tamaño4 = nombre4.length();
         System.out.println("el nombre mas largo es "+nombre4+" y tiene "+tamaño4+" letras");
         for (int i = 0; i < nombre1.length(); i++) {
